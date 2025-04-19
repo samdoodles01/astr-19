89aebff19b469786ce54eb275338990937695ded
samdoodles01 <samilton@ucsc.edu>
2025-04-19 10:42:22 -0700

    Add files via upload

---
+++ Code_Journal#1_Prompt#3.py
@@ -0,0 +1,16 @@
+#Prompt 3 
+#Define the funtion  f(x) that returns x**3 + 8.
+def f(x):
+	return x**3 + 8 
+
+def main():
+	x = 9
+	result = f(x)
+	print("f(x)=", result)
+
+#Is vaule greater than 27?
+	if result > 27:
+		print ("YAY!")
+
+#Run function 
+main()
