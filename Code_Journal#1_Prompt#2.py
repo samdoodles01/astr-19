edb820720d3ffe25aa7123549ee14c7ba752ffb3
samdoodles01 <samilton@ucsc.edu>
2025-04-19 10:18:46 -0700

    Add files via upload

---
+++ Code_Journal#1_Prompt#2.py
@@ -0,0 +1,22 @@
+#Prompt 2 
+
+# 1. Sum of two floating point numbers
+float1 = 2.3
+float2 = 7.7
+sum_result = float1 + float2
+print("Sum of floats:", sum_result)
+print("Data type:", type(sum_result))
+
+# 2. Difference between two integers
+int1 = 7
+int2 = 5
+difference_result = int1 - int2
+print("\nDifference of integers:", difference_result)
+print("Data type:", type(difference_result))
+
+# 3. Product of a floating point number and an integer
+float3 = 10
+int3 = 2
+product_result = float3 * int3
+print("\nProduct of float and integer:", product_result)
+print("Data type:", type(product_result))
